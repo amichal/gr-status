b896f70e3b133047f23534f6a4beaefd25ed490d
Aaron Michal <amichal@greenriver.org>
2013-03-08 13:52:31 -0500

    more formal names

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,28 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 clients = Client.create!([
-	{ :name => 'Starbucks', :logo_url => '/icons/Starbucks.gif', :web_sites_attributes => [
+	{ :name => 'Starbucks Coffee Company', :logo_url => '/icons/Starbucks.gif', :web_sites_attributes => [
 		{ :url => 'http://demo.cafepractices.info'}, 
 		{ :url => 'http://cafepractices.info'}
 		]
 	}, 
-	{ :name => 'ERS', :logo_url => '/icons/ERS.png', :web_sites_attributes => [
+	{ :name => 'Education Resource Strategies', :logo_url => '/icons/ERS.png', :web_sites_attributes => [
 		{ :url => 'http://www.system2020.org/'}, 
 		{ :url => 'http://www.erstools.org/dream'}, 
 		{ :url => 'http://www.erstools.org/hold-em'}, 
 		{ :url => 'http://www.erstools.org/assessments/resource_check'}
 		]
 	}, 
-	{ :name => "Author's Guild", :logo_url => "/icons/Author's Guild.png", :web_sites_attributes => [
+	{ :name => "The Author's Guild", :logo_url => "/icons/Author's Guild.png", :web_sites_attributes => [
 		{ :url => 'http://www.authorsguild.org'}, 
 		{ :url => 'http://www.authorsguild.net'}
 		]
 	}, 
-	{ :name => 'Noveda', :logo_url => '/icons/Noveda.png', :web_sites_attributes => [
+	{ :name => 'Noveda Technologies', :logo_url => '/icons/Noveda.png', :web_sites_attributes => [
 		{ :url => 'http://www.makemesustainable.com'}
     ]
 	}, 
-	{ :name => 'Bibme', :logo_url => '/icons/Bibme.png', :web_sites_attributes => [
+	{ :name => 'BibMe', :logo_url => '/icons/Bibme.png', :web_sites_attributes => [
 		{ :url => 'http://www.bibme.org'}
 		]
 	}, 
@@ -45,23 +45,23 @@ clients = Client.create!([
 	}, { :name => 'FCA', :logo_url => '/icons/FCA.jpeg', :web_sites_attributes => [
 		{ :url => 'http://www.foundationforcontemporaryarts.org'}
 		]
-	}, { :name => 'UVA', :logo_url => 'http://placehold.it/100x100', :web_sites_attributes => [
+	}, { :name => 'University of Virgina', :logo_url => 'http://placehold.it/100x100', :web_sites_attributes => [
 		{ :url => 'http://canlead.net'}, 
 		{ :url => 'http://dev.canlead.net'}, 
 		{ :url => 'http://course.canlead.net'}, 
 		{ :url => 'http://www.etips.info'}
 		]
-	}, { :name => 'KCS', :logo_url => '/icons/KCS.png', :web_sites_attributes => [
+	}, { :name => 'Kidz Central Station, LLC', :logo_url => '/icons/KCS.png', :web_sites_attributes => [
 		{ :url => 'https://www.kidzcentralstation.com'}
     ]
-	}, { :name => 'VT', :logo_url => '/icons/VT.gif', :web_sites_attributes => [
+	}, { :name => 'VT Midi project', :logo_url => '/icons/VT.gif', :web_sites_attributes => [
 		{ :url => 'http://www.vtmidi.org'}
 		]
 	}, { :name => 'Learning Forward', :logo_url => '/icons/Learning Forward.gif', :web_sites_attributes => [
 		{ :url => 'http://www.sai-learningforward.org'}
 		]
-	}, { :name => 'YSA', :logo_url => '/icons/YSA.png', :web_sites_attributes => [
-		{ :url => 'http://members.ysaleagues.com'}
+	}, { :name => 'Yorkville Sports Association', :logo_url => '/icons/YSA.png', :web_sites_attributes => [
+		{ :url => 'https://members.ysaleagues.com'}
 		]
 	}, { :name => 'NEA', :logo_url => '/icons/NEA.gif', :web_sites_attributes => [
 		{ :url => 'http://www.keysonline.org'}
