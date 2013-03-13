4f69e96f2eaf7774286cf8e210d743f2442b8a70
Ian Mahoney <imahoney@marlboro.edu>
2013-03-13 13:53:47 -0400

    working on #9.  Added a random number generator to the Client model.

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,4 +7,8 @@ class Client < ActiveRecord::Base
   has_many :web_sites, :dependent=>:destroy, :inverse_of=>:client
 
   accepts_nested_attributes_for :web_sites
+
+  def hours
+  	return Random.new.rand(10..40)
+  end
 end
