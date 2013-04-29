82812cac7366be84b3a91271c0f3eda6568fa9e8
Ian Mahoney <imahoney@marlboro.edu>
2013-04-29 16:21:00 -0400

    Removing old unnecessary functions from Client and Website.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,14 +1,9 @@
 class Client < ActiveRecord::Base
-  attr_accessible :logo_url, :name, :as=>:admin
-  attr_accessible :logo_url, :name, :web_sites_attributes, :description, :harvest_client_id
+  attr_accessible :logo_url, :name, :web_sites_attributes, :description
 
   validates :name, :presence => true, :uniqueness=>true
 
   has_many :web_sites, :dependent=>:destroy, :inverse_of=>:client
 
   accepts_nested_attributes_for :web_sites
-
-  def hours
-  	return Random.new.rand(10..40)
-  end
 end
--- app/models/web_site.rb
+++ app/models/web_site.rb
@@ -1,6 +1,5 @@
 class WebSite < ActiveRecord::Base
   attr_accessible :monitored, :url, :client_id, :ga_profile_id, :git_path
-  attr_accessible :monitored, :url, :client_id, :ga_profile_id, :git_path, :as=>:admin
 
   validates :url, :presence => true, :uniqueness=>true
 
@@ -13,27 +12,4 @@ class WebSite < ActiveRecord::Base
   def path
     URI.parse(self.url).path
   end
-
-  def requests_per_minute
-    prng = Random.new
-    start = prng.rand(0...600)
-    last = start
-    d = (0..15).map do
-       if prng.rand(100) > 98
-         nil
-       else
-         last += prng.rand(-30...30)
-       end  
-    end
-    d << {
-        :y => d.last,
-        :dataLabels => {
-          :enabled =>  true,
-          :align => 'left',
-          :crop => 'false',
-          :x => 3,
-          :verticalAlign => 'middle'
-        }
-      }
-  end 
 end
