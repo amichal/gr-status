74551b2a44c802166076b6e986988cb090905a67
Ian Mahoney <imahoney@marlboro.edu>
2013-04-30 15:59:00 -0400

    Added a checkbox to the edit form which allows the deletion of websites.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,10 @@
 class Client < ActiveRecord::Base
-  attr_accessible :logo_url, :name, :web_sites_attributes, :description
+  attr_accessible :logo_url, :name, :web_sites_attributes, :description, :harvest_client_id
 
   validates :name, :presence => true, :uniqueness=>true
 
   has_many :web_sites, :dependent=>:destroy, :inverse_of=>:client
 
-  accepts_nested_attributes_for :web_sites, 
+  accepts_nested_attributes_for :web_sites, :allow_destroy => true, 
     :reject_if => proc { |attributes| attributes['url'].blank? }
 end
